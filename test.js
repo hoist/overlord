e623fafd4181a18e39e7d6c44ae9f7102fbf57aa
Owen Evans <owen@bgeek.net>
2015-02-03 15:51:00 +1300

    add scaling group scripts

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
-var task = require('./lib/ec2_check_status');
-task();
+var task = require('./lib/create_executor_launch_config');
+task('ami-253d6615', 420, '49c00e706f0d2ff66ae7b5ef0db4545bcc8312b3');
