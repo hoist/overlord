03cb44114163f752a7626baf7fc3cbc985965b1e
Owen Evans <owen@bgeek.net>
2015-03-02 08:40:13 -0800

    add prune new relic server task

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var chefJob = require('./lib/chef_task');
 var countQueuesJob = require('./lib/count_queues_task');
 var checkEC2Job = require('./lib/ec2_check_status');
 var rebalanceJob = require('./lib/rebalance_executors_task');
+var pruneNewRelicJob = require('./lib/prune_new_relic');
+
 agenda.database(config.get('Hoist.overlord.mongo.db'), 'operational-jobs');
 logger.info('starting server');
 logger.info('registering chef maintainance job');
@@ -20,13 +22,24 @@ agenda.define('rebalance executors', {
   logger.info('starting rebalance job');
   rebalanceJob().nodeify(done);
 });
-agenda.define('count queues', function (job, done) {
+agenda.define('count queues', {
+  lockLifetime: 10000
+}, function (job, done) {
   logger.info('starting count queues job');
   countQueuesJob().nodeify(done);
 });
-agenda.define('check ec2 instances', function (job, done) {
+agenda.define('check ec2 instances', {
+  lockLifetime: 10000
+}, function (job, done) {
   logger.info('starting ec2 check job');
   checkEC2Job().nodeify(done);
+});
+
+agenda.define('prune new relic servers', {
+  lockLifetime: 10000
+}, function (job, done) {
+  logger.info('starting new relic prune job');
+  pruneNewRelicJob().nodeify(done);
 });
 
 logger.info('registering schedule');
@@ -34,6 +47,7 @@ agenda.every('2 minutes', 'maintain chef nodes');
 agenda.every('1 minute', 'count queues');
 agenda.every('5 minutes', 'check ec2 instances');
 agenda.every('2 minutes', 'rebalance executors');
+agenda.every('5 minutes', 'prune new relic servers');
 
 logger.info('starting agenda');
 agenda.start();
