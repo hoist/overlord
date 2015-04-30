16445be94a1dbe08062d0f17c64a3a3f3d4bf771
Owen Evans <owen@bgeek.net>
2015-05-01 10:51:48 +1200

    update build

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
+process.env.NODE_ENV = 'production_test';
 var task = require('./lib/create_executor_launch_config');
-task('ami-253d6615', 420, '49c00e706f0d2ff66ae7b5ef0db4545bcc8312b3');
+task('ami-cf2d1bff', 506, '802fc52d0530340be59dd7e5fe30c4deefbdab73');
