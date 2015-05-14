d6bca50d09803553b550131dfb2698ee2065ad0b
Owen Evans <owen@bgeek.net>
2015-05-15 08:51:12 +1200

    add task service

--- launch.sh
+++ launch.sh
@@ -1,5 +1,8 @@
-fleetctl unload overlord-web@{1..3}.service && \
-fleetctl destroy overlord-web@{1..3}.service && \
-fleetctl destroy overlord-web@.service && \
-fleetctl submit ./overlord-web\@.service && \
+#!/bin/bash
+
+fleetctl destroy overlord-web@.service
+fleetctl destroy overlord-web@{1..3}.service
 fleetctl start overlord-web@{1..3}.service
+fleetctl destroy overlord-task@.service
+fleetctl destroy overlord-task@{1..3}.service
+fleetctl start overlord-task@{1..3}.service
