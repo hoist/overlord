93178df4db56eae8697dab0d0cd39c379070716c
Owen Evans <owen@hoist.io>
2015-05-26 11:45:08 +1200

    add migrations

---
+++ migrations/2015-05-26-1136-add-api-token.js
@@ -0,0 +1,25 @@
+'use strict';
+var APIToken = require('../lib/models/api_token');
+exports.up = function (next) {
+	console.log('    --> This is migration 2015-05-26-1136-add-api-token.js being applied');
+	var apiToken = new APIToken({
+		name: 'Circle CI',
+		token: 'xc1zrzacmxp9samlj9nya4gzay'
+	});
+	apiToken.saveAsync()
+		.spread(function (token) {
+			console.log('created CircleCI api token:', token.token);
+			next();
+		});
+};
+
+
+exports.down = function (next) {
+	console.log('    --> This is migration 2015-05-26-1136-add-api-token.js being rollbacked');
+	APIToken.removeAsync({
+			name: 'Circle CI'
+		})
+		.then(function () {
+			next();
+		});
+};
--- tests/web_app/integration_tests/create_project_api_tests.js
+++ tests/web_app/integration_tests/create_project_api_tests.js
@@ -21,7 +21,9 @@ describe('create project API', function () {
 			configureServer().then((s) => {
 				server = Bluebird.promisifyAll(s);
 			}),
-			new APIToken().saveAsync().spread((token) => {
+			new APIToken({
+				name: 'unit test token'
+			}).saveAsync().spread((token) => {
 
 				apiToken = token;
 			})
@@ -35,7 +37,7 @@ describe('create project API', function () {
 			return mongoose.disconnectAsync();
 		});
 	});
-	describe('POST to /API/build/new%20project', () => {
+	describe('POST to /API/build/new project', () => {
 		let response;
 		before(function () {
 			return new Promise((resolve) => {
@@ -55,6 +57,9 @@ describe('create project API', function () {
 				response = res;
 			});
 		});
+		after(() => {
+			return Project.removeAsync({});
+		});
 		it('returns [OK]', () => {
 			expect(response.statusCode).to.eql(200);
 		});
@@ -64,9 +69,36 @@ describe('create project API', function () {
 			}).then((project) => expect(project).to.exist);
 		});
 	});
-	describe('unathenticated POST to /API/build/new%20project', () => {
-		it('returns [unathenticated]', () => {
-
+	describe('unathenticated POST to /API/build/new project', () => {
+		let response;
+		before(function () {
+			return new Promise((resolve) => {
+				server.inject({
+					method: 'POST',
+					url: `/api/build/new project?api_token=bad_token`,
+					payload: {
+						build: 22,
+						branch: 'feature/somebranch',
+						sha1: 'sha1',
+						compareUrl: 'http://compare'
+					}
+				}, (res) => {
+					resolve(res);
+				});
+			}).then((res) => {
+				response = res;
+			});
+		});
+		after(() => {
+			return Project.removeAsync({});
+		});
+		it('returns [Unauthorized]', () => {
+			expect(response.statusCode).to.eql(401);
+		});
+		it('doesn\'t create the project', () => {
+			return Project.findOneAsync({
+				name: 'new project'
+			}).then((project) => expect(project).to.not.exist);
 		});
 	});
 });
