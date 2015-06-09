0b3994ea90a9e6a16aa6c7aeb20f204cae15b048
Owen Evans <owen@hoist.io>
2015-06-09 12:33:21 +1200

    make the login form a bit more usable

--- lib/web_app/views/session/components/login.jsx
+++ lib/web_app/views/session/components/login.jsx
@@ -3,14 +3,27 @@ import React from 'react';
 class LoginForm extends React.Component {
   render() {
     return (
-      <div className="form">
-        <div className="form-group">
-          <form className="login-form" method="post">
-            <input id="username" name="username" placeholder="Username/Email Address" required type="email"/>
-            <input id="password" name="password" placeholder="Password" required type="password"/>
-            <input id="submit" type="submit" value="Sign In"/>
-          </form>
-        </div>
+      <div className="container">
+            <div className="login-panel panel panel-default col-xs-offset-3 col-xs-6">
+              <div className="panel-heading">
+                <img className="login-icon" />
+              </div>
+              <div className="panel-body">
+                <form className="form-horizontal" method="post">
+                  <div className='form-group'>
+                    <input className="form-control" id="username" name="username" placeholder="Username/Email Address" required type="email"/>
+                  </div>
+                  <div className="form-group">
+                      <input className="form-control" id="password" name="password" placeholder="Password" required type="password"/>
+                  </div>
+                  <div className="form-group">
+                    <div className="col-xs-offset-4 col-sm-4">
+                      <button className="btn btn-default" id="submit" type="submit" >Sign In</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
       </div>
     );
   }
