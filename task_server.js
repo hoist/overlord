21e927ce8d02922c8f40d5b082cefe260de7b85c
Owen Evans <owen@hoist.io>
2015-08-02 16:59:52 -0700

    trying again to fix scaling task

--- task_server.js
+++ task_server.js
@@ -83,7 +83,7 @@ logger.info('registering schedule');
 agenda.every('2 minutes', 'maintain chef nodes');
 agenda.every('1 minute', 'count queues');
 agenda.every('10 seconds', 'rebalance executors');
-agenda.every('1 minute', 'scale executors');
+agenda.every('5 minute', 'scale executors');
 agenda.every('5 minutes', 'prune new relic servers');
 agenda.every('30 minutes', 'prune rabbitmq queues');
 agenda.every('5 minutes', 'check executor health');
