b7444ffb15b3efe98f3aa41d87c365c07383e89b
Owen Evans <owen@hoist.io>
2015-08-10 10:38:34 -0700

    adding healthcheck as a separate service

--- task_server.js
+++ task_server.js
@@ -7,27 +7,16 @@ var config = require('config');
 var agenda = new Agenda();
 var pruneNewRelicJob = require('./lib/tasks/prune_new_relic_task');
 var cullQueuesTask = require('./lib/tasks/cull_queues_task');
-var bluebird = require('bluebird');
 
-var HealthChecker = require('./lib/tasks/health_checker');
 var mongoConnection = require('./lib/mongoose_connection');
 
 mongoConnection.connect();
 
 agenda.database(config.get('Hoist.mongo.overlord'), 'operational-jobs');
 
-var healthChecker = new HealthChecker();
 
 logger.info('starting server');
 logger.info('registering chef maintainance job');
-
-agenda.define('check executor health', {
-  lockLifetime: 10000
-}, function (job, done) {
-  logger.info('starting check health job');
-  bluebird.resolve(healthChecker.check()).nodeify(done);
-
-});
 
 agenda.define('prune new relic servers', {
   lockLifetime: 10000
@@ -46,7 +35,6 @@ logger.info('registering schedule');
 
 agenda.every('5 minutes', 'prune new relic servers');
 agenda.every('30 minutes', 'prune rabbitmq queues');
-agenda.every('5 minutes', 'check executor health');
 
 
 logger.info('starting agenda');
