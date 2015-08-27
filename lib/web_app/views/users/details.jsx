3bb98ccc799b419c12015591648b348b55e0ad8a
Jamie Wilson <jamie.wilsons@gmail.com>
2015-08-26 21:40:51 -0700

    shows user raw json

--- lib/web_app/views/users/details.jsx
+++ lib/web_app/views/users/details.jsx
@@ -5,10 +5,19 @@ import Page from '../_components/page.jsx';
 
 class DetailsPage extends React.Component {
   render () {
+    function createMarkup (fields) {
+      return {__html: JSON.stringify(fields)}; 
+    }
 
     return (
       <Page {...this.props}>
         <div className="container">
+          <h3>User</h3>
+          <pre dangerouslySetInnerHTML={createMarkup(this.props.rawUser)}></pre>
+          <h3>Organisations</h3>
+          <pre dangerouslySetInnerHTML={createMarkup(this.props.organisations)}></pre>
+          <h3>Applications</h3>
+          <pre dangerouslySetInnerHTML={createMarkup(this.props.applications)}></pre>
         </div>
       </Page>
     );
