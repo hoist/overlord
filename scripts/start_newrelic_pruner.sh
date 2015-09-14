04fa329262bf2e3b02b655e42ba6feb400181473
Owen Evans <owen@hoist.io>
2015-09-14 14:21:08 +1200

    create newrelic pruner task as timer

---
+++ scripts/start_newrelic_pruner.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set +e
+
+#setup config
+echo "setting up app config"
+cp /config/production.json ./config/production.json
+
+#start app
+echo "starting app"
+babel-node ./lib/tasks/new_relic_pruner.js
