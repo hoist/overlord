8c1c12773b4c098e213caae00321d01ab9f33354
Owen Evans <owen@hoist.io>
2015-10-26 20:06:36 +1300

    unit file tweeks

--- scripts/start_rebalancer.sh
+++ scripts/start_rebalancer.sh
@@ -7,4 +7,4 @@ cp /config/production.json ./config/production.json
 
 #start app
 echo "starting app"
-for ((n=0;n<1000;n++)); do bash -c "babel-node ./lib/tasks/rebalancer.js"; done
+babel-node ./lib/tasks/rebalancer.js
